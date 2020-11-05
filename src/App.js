9a8d940bd5fccb9e866ad47430dbc8ec131d8779
Danilo Aleixo <danilo_aleixo@hotmail.com>
2020-11-04 22:03:07 -0300

    Putting maerial UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
     return (
-        <div>Hello World!</div>
+        <div>
+            Hello World!
+            <Button color="primary">Hello World</Button>
+        </div>
     );
 }
 export default App;
